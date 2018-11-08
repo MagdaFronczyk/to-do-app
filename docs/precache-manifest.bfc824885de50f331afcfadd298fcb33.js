ef2069b23b5c5486d8cd036b93917d6caa768bc3
MagdaFronczyk <magdalenaoliwiaf@gmail.com>
2018-11-08 12:05:34 +0100

    created docs folder

---
+++ docs/precache-manifest.bfc824885de50f331afcfadd298fcb33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ceb1c93bcfaef7d993a116808d1d00a",
+    "url": "./static/media/body_background.5ceb1c93.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "cea690adb2cb82002c5d",
+    "url": "./static/js/main.cea690ad.chunk.js"
+  },
+  {
+    "revision": "d5d92a70f636de8410df",
+    "url": "./static/js/1.d5d92a70.chunk.js"
+  },
+  {
+    "revision": "cea690adb2cb82002c5d",
+    "url": "./static/css/main.a908a9e9.chunk.css"
+  },
+  {
+    "revision": "55b0d82c8f48dda0caa799b98199fb40",
+    "url": "./index.html"
+  }
+];
